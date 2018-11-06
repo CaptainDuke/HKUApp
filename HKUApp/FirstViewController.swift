73093f989e79e970894d4e48a010260620f95907
Duke <captainduke328@gmail.com>
2018-11-06 14:48:16 +0800

    Initial Commit

---
+++ HKUApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  HKUApp
+//
+//  Created by Captwang on 6/11/2018.
+//  Copyright © 2018 Captwang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ HKUApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  HKUApp
+//
+//  Created by Captwang on 6/11/2018.
+//  Copyright © 2018 Captwang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
