57ee70ff1f010fbccf6a34fff34bda2f496be383
Duke <captainduke328@gmail.com>
2018-11-07 20:45:35 +0800

    Portrait and PopUp

---
+++ HKUApp/ProfInfoVC.swift
@@ -0,0 +1,23 @@
+//
+//  ProfInfoVC.swift
+//  HKUApp
+//
+//  Created by Captwang on 7/11/2018.
+//  Copyright © 2018 Captwang. All rights reserved.
+//
+
+import UIKit
+
+class ProfInfoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+ 
+    
+    @IBAction func dismissPopup(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+}
