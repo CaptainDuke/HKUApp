dbdb11174f923f0f586f05f9518b56b41c56b437
Duke <captainduke328@gmail.com>
2018-11-07 19:57:15 +0800

    UIbutton

--- IBDesignables/DesignableButton.swift
+++ IBDesignables/DesignableButton.swift
@@ -10,16 +10,21 @@ import UIKit
 
 @IBDesignable class DesignableButton: UIButton {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-    var borderWidth: CGFloat = 0.0 {
+ 
+    @IBInspectable var borderWidth: CGFloat = 0.0 {
         didSet{
             self.layer.borderWidth = borderWidth
+        }
+    }
+    @IBInspectable var borderColor: UIColor = UIColor.clear{
+        didSet{
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat = 0{
+        didSet{
+            self.layer.cornerRadius = cornerRadius
         }
     }
 
